78af884f0fcfb3af99ba4d1724197af4e8995b45
Nate Hanisch <nathan.hanisch86@gmail.com>
2026-05-05 19:31:17 -0500

    Add Recipe model class

---
+++ RecipeFinder/Models/Recipe.cs
@@ -0,0 +1,19 @@
+namespace RecipeFinder.Models
+{
+    public class Recipe
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string SourceUrl { get; set; }
+        public int TimeToMake { get; set; }
+        public List<string> Cuisines { get; set; }
+        public List<string> Diets { get; set; }
+        public string Summary { get; set; }
+
+        public Recipe()
+        {
+            Cuisines = new List<string>();
+            Diets = new List<string>();
+        }
+    }
+}
